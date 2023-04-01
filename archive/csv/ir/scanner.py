440f98f6d317612b2134183ccb93f62a74f3119e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-04-01 12:15:12 -0400

    Add ir scanner and parser skeletons
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

---
+++ archive/csv/ir/scanner.py
@@ -0,0 +1,21 @@
+from os import scandir
+from pathlib import Path
+
+from archive.csv.ir.transaction import IRColumns
+from archive.csv.tools.io import read_csv
+
+
+def get_merged_ir_dataset(dir_path: str | Path) -> list[list[str]]:
+    header: list[list[str]] = []
+    table: list[list[str]] = []
+
+    for entry in scandir(dir_path):
+        if entry.is_file:
+            csv_table = read_csv(entry.path)
+            if not header:
+                header = [csv_table[0]]
+            table.extend(csv_table[1:])
+
+    sorted_table = sorted(table, key=lambda row: row[IRColumns.DATETIME.value])
+
+    return header + sorted_table
