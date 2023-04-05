6025671ab3f1532196f34276a8f02a04f6f9da73
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-04-05 16:21:48 -0400

    Fix exchange import modules
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- archive/ir/config.py
+++ archive/ir/config.py
@@ -1,9 +1,9 @@
-from archive.coinbase.ir import build_coinbase_ir
-from archive.coinbase.scanner import scan_coinbase
-from archive.coinbase_pro.ir import build_coinbase_pro_ir
-from archive.coinbase_pro.scanner import scan_coinbase_pro
-from archive.kraken.ir import build_kraken_ir
-from archive.kraken.scanner import scan_kraken
+from archive.exchange.coinbase.ir import build_coinbase_ir
+from archive.exchange.coinbase.scanner import scan_coinbase
+from archive.exchange.coinbase_pro.ir import build_coinbase_pro_ir
+from archive.exchange.coinbase_pro.scanner import scan_coinbase_pro
+from archive.exchange.kraken.ir import build_kraken_ir
+from archive.exchange.kraken.scanner import scan_kraken
 
 # Configuration dictionary for each exchange
 exchanges = {
