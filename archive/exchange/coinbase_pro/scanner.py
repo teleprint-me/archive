604c41dbbff40c658e9c7e262aa62039af11d37f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-04-09 20:50:32 -0400

    Refactor Coinbase Pro scanner and remove unnecessary functions
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- archive/exchange/coinbase_pro/scanner.py
+++ archive/exchange/coinbase_pro/scanner.py
@@ -7,41 +7,6 @@ from archive.exchange.coinbase_pro.models import (
 from archive.tools.io import read_csv
 
 
-def get_coinbase_pro_transaction(csv_row: list[str]) -> CoinbaseProTransaction:
-    total = float(csv_row[CoinbaseProColumns.TOTAL.value])
-    return CoinbaseProTransaction(
-        portfolio=csv_row[CoinbaseProColumns.PORTFOLIO.value],
-        trade_id=int(csv_row[CoinbaseProColumns.TRADE_ID.value]),
-        product=csv_row[CoinbaseProColumns.PRODUCT.value],
-        side=csv_row[CoinbaseProColumns.SIDE.value],
-        created_at=csv_row[CoinbaseProColumns.CREATED_AT.value],
-        size=float(csv_row[CoinbaseProColumns.SIZE.value]),
-        size_unit=csv_row[CoinbaseProColumns.SIZE_UNIT.value],
-        price=float(csv_row[CoinbaseProColumns.PRICE.value]),
-        fee=float(csv_row[CoinbaseProColumns.FEE.value]),
-        total=total if total > 0 else -total,
-        total_unit=csv_row[CoinbaseProColumns.TOTAL_UNIT.value],
-    )
-
-
-def get_coinbase_pro_csv_row(
-    coinbase_pro_transaction: CoinbaseProTransaction,
-) -> list[str]:
-    return [
-        coinbase_pro_transaction.portfolio,
-        str(coinbase_pro_transaction.trade_id),
-        coinbase_pro_transaction.product,
-        coinbase_pro_transaction.side,
-        coinbase_pro_transaction.created_at,
-        str(coinbase_pro_transaction.size),
-        coinbase_pro_transaction.size_unit,
-        str(coinbase_pro_transaction.price),
-        str(coinbase_pro_transaction.fee),
-        str(coinbase_pro_transaction.total),
-        str(coinbase_pro_transaction.total_unit),
-    ]
-
-
 def build_coinbase_pro_csv(
     transactions: list[CoinbaseProTransaction],
 ) -> list[list[str]]:
@@ -62,8 +27,20 @@ def build_coinbase_pro_csv(
         ]
     ]
     csv_table = []
-    for row in transactions:
-        transaction = get_coinbase_pro_csv_row(row)
+    for tx in transactions:
+        transaction = [
+            tx.portfolio,
+            str(tx.trade_id),
+            tx.product,
+            tx.side,
+            tx.created_at,
+            str(tx.size),
+            tx.size_unit,
+            str(tx.price),
+            str(tx.fee),
+            str(tx.total),
+            str(tx.total_unit),
+        ]
         csv_table.append(transaction)
     return csv_header + csv_table
 
@@ -74,12 +51,25 @@ def build_coinbase_pro_transactions(
     transactions = []
     # omit the header from the conversion process
     for csv_row in csv_table[1:]:
-        transaction = get_coinbase_pro_transaction(csv_row)
+        total = float(csv_row[CoinbaseProColumns.TOTAL.value])
+        transaction = CoinbaseProTransaction(
+            portfolio=csv_row[CoinbaseProColumns.PORTFOLIO.value],
+            trade_id=int(csv_row[CoinbaseProColumns.TRADE_ID.value]),
+            product=csv_row[CoinbaseProColumns.PRODUCT.value],
+            side=csv_row[CoinbaseProColumns.SIDE.value],
+            created_at=csv_row[CoinbaseProColumns.CREATED_AT.value],
+            size=float(csv_row[CoinbaseProColumns.SIZE.value]),
+            size_unit=csv_row[CoinbaseProColumns.SIZE_UNIT.value],
+            price=float(csv_row[CoinbaseProColumns.PRICE.value]),
+            fee=float(csv_row[CoinbaseProColumns.FEE.value]),
+            total=total if total > 0 else -total,
+            total_unit=csv_row[CoinbaseProColumns.TOTAL_UNIT.value],
+        )
         transactions.append(transaction)
     return transactions
 
 
-def scan_coinbase_pro(
+def scan_coinbase_pro_fills(
     filepath: str | Path,
 ) -> list[CoinbaseProTransaction]:
     csv_table = read_csv(filepath)
