e700f45f9a3fc39f9e8588c0fa24848ea31f82ac
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-04-18 19:22:29 -0400

    chore: Bump revision from 0.1.6 to 0.1.7
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- archive/exchange/coinbase/api.py
+++ archive/exchange/coinbase/api.py
@@ -23,7 +23,7 @@ __agent__: str = "teleprint-me/archive"
 __source__: str = f"https://github.com/{__agent__}"
 
 # Source version.
-__version__: str = "0.1.6"
+__version__: str = "0.1.7"
 
 # Rate limit of API requests in seconds.
 # Calculated as 1 / (10000 requests per hour / 3600 seconds per hour)
