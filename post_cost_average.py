5cbebc04ce01c1b7ff7dd5f07afe523824efb712
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-04-19 22:50:38 -0400

    chore: Fix help strings
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

---
+++ post_cost_average.py
@@ -0,0 +1,35 @@
+import sys
+from argparse import ArgumentParser, Namespace
+
+from archive.average.cost import execute_cost_average
+
+
+def get_arguments() -> Namespace:
+    parser = ArgumentParser(description="Post Cost Average Orders.")
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        default="data/average/cost_average_records.csv",
+        help="File to read from and write to (default: cost_average_records.csv)",
+    )
+
+    parser.add_argument(
+        "-x",
+        "--execute",
+        action="store_true",  # NOTE: Ensure this is store_true!
+        help="Execute order based on set environment variables",
+    )
+
+    return parser.parse_args(sys.argv[1:])
+
+
+def main():
+    args = get_arguments()
+
+    execute_cost_average(args.file, args.execute)
+
+
+if __name__ == "__main__":
+    main()
