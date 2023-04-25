746b4c12e9b2ddefab2f9cfe90d63904e71e8364
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-04-25 11:33:03 -0400

    refactor: Add dynamic averaging columns and fix dataclasses
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- archive/average/models.py
+++ archive/average/models.py
@@ -2,22 +2,6 @@ from dataclasses import dataclass
 from enum import Enum
 
 
-class AverageColumn(Enum):
-    """Enumerated columns for averaging strategies records."""
-
-    EXCHANGE = 0
-    PRODUCT_ID = 1
-    PRINCIPAL_AMOUNT = 2
-    SIDE = 3
-    DATETIME = 4
-    MARKET_PRICE = 5
-    CURRENT_TARGET = 6
-    CURRENT_VALUE = 7
-    ORDER_SIZE = 8
-    TOTAL_ORDER_SIZE = 9
-    INTERVAL = 10
-
-
 class CostAverageColumn(Enum):
     """Enumerated columns for cost averaging strategy records."""
 
@@ -33,6 +17,44 @@ class CostAverageColumn(Enum):
     TOTAL_ORDER_SIZE = 9
     INTERVAL = 10
     GAIN_OR_LOSS = 11
+
+
+class DynamicAverageColumn(Enum):
+    """Enumerated columns for dynamic averaging strategy records."""
+
+    EXCHANGE = 0
+    PRODUCT_ID = 1
+    PRINCIPAL_AMOUNT = 2
+    SIDE = 3
+    DATETIME = 4
+    MARKET_PRICE = 5
+    CURRENT_TARGET = 6
+    CURRENT_VALUE = 7
+    ORDER_SIZE = 8
+    TOTAL_ORDER_SIZE = 9
+    INTERVAL = 10
+    MULTIPLIER = 11
+    TRADE_AMOUNT = 12
+    TOTAL_TRADE_AMOUNT = 13
+
+
+class ValueAverageColumn(Enum):
+    """Enumerated columns for value averaging strategy records."""
+
+    EXCHANGE = 0
+    PRODUCT_ID = 1
+    PRINCIPAL_AMOUNT = 2
+    SIDE = 3
+    DATETIME = 4
+    MARKET_PRICE = 5
+    CURRENT_TARGET = 6
+    CURRENT_VALUE = 7
+    ORDER_SIZE = 8
+    TOTAL_ORDER_SIZE = 9
+    INTERVAL = 10
+    GROWTH_RATE = 11
+    TRADE_AMOUNT = 12
+    TOTAL_TRADE_AMOUNT = 13
 
 
 @dataclass
@@ -51,29 +73,27 @@ class AverageRecord:
     total_order_size: float
     interval: int = 0
 
-    @property
-    def base(self) -> str:
-        return self.product_id.split("-")[0]
-
-    @property
-    def quote(self) -> str:
-        return self.product_id.split("-")[1]
-
 
 @dataclass
 class CostAverageRecord(AverageRecord):
+    """A dataclass representing a cost averaging record."""
+
     gain_or_loss: float = 0
 
 
 @dataclass
 class DynamicAverageRecord(AverageRecord):
-    factor: float = 0
-    factor_amount: float = 0
-    total_factor_amount: float = 0
+    """A dataclass representing a dynamic averaging record."""
+
+    multiplier: float = 0
+    trade_amount: float = 0
+    total_trade_amount: float = 0
 
 
 @dataclass
 class ValueAverageRecord(AverageRecord):
-    interest_rate: float = 0
+    """A dataclass representing a value averaging record."""
+
+    growth_rate: float = 0
     trade_amount: float = 0
     total_trade_amount: float = 0
